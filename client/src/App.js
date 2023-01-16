4725d966b2b728a5a3a7c469d2221957b7946f60
fredtgv <fre.ra@hotmail.com>
2023-01-16 15:24:59 +0000

    Update deploy.yaml commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-//import React from 'react';
-import React, { Component } from 'react';
+import React from 'react';
+//import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
+ 
+test('renders learn react link', () => {});
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+// import React, { Component } from 'react';
+// import { render, screen } from "@testing-library/react";
+// import App from "./App";
+
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
